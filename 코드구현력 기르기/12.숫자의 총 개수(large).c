d41e80270b63c9aac01ebf8f76cc2f981ac724ec
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2022-09-04 23:48:18 +0900

    Update 12.숫자의 총 개수(large).c

--- 코드구현력 기르기/12.숫자의 총 개수(large).c
+++ 코드구현력 기르기/12.숫자의 총 개수(large).c
@@ -1,4 +1,29 @@
 #include<stdio.h>
+//#include<string.h>
+int main(){
+	//freopen("input.txt","rt",stdin);
+	int n, i, cnt=0, tmp;
+	scanf("%d", &n);
+	for(i=1; i<=n; i++){
+		tmp = i;
+		if(i<10)cnt++;
+		else if(i<100)cnt+=2;
+		else if(i<1000)cnt+=3;
+		else if(i<10000)cnt+=4;
+		else if(i<100000)cnt+=5;
+		else if(i<1000000)cnt+=6;
+		else if(i<10000000)cnt+=7;
+		else if(i<100000000)cnt+=8;
+		else if(i<1000000000)cnt+=9;
+		else if(i<10000000000)cnt+=10;
+			
+	}
+	printf("%d",cnt);
+	return 0;
+	
+}
+
+#include<stdio.h>
 int main(){
 	freopen("input.txt", "rt", stdin);
 	int n, sum=0, cnt=1, digit=9, res=0;
