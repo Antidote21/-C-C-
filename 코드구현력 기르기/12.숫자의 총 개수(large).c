b04c050b63b99e88fbf65bc85371fae6a6e71224
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2022-11-02 00:59:01 +0900

    Update 12.숫자의 총 개수(large).c

--- 코드구현력 기르기/12.숫자의 총 개수(large).c
+++ 코드구현력 기르기/12.숫자의 총 개수(large).c
@@ -57,3 +57,16 @@ int main(){
 	return 0;
 }
 
+#include <iostream>
+#include<cmath> 
+using namespace std; 
+
+int main() {
+	int n;
+	int total=0;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		total += int(log10(i))+1;
+	}
+	cout << total;
+}
