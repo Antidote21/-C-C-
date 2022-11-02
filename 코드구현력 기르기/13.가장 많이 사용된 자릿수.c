85a66425fefebcd768e758ae4f4b4804ed3c5f3d
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2022-11-02 16:33:18 +0900

    Update 13.가장 많이 사용된 자릿수.c

--- 코드구현력 기르기/13.가장 많이 사용된 자릿수.c
+++ 코드구현력 기르기/13.가장 많이 사용된 자릿수.c
@@ -20,5 +20,32 @@ int main(){
 	return 0;
 }
 
+#include <iostream>
+#include<string> 
+using namespace std; 
 
+int main() {
+	int n;
+	int cnt = 0;
+	int a[10]={0};
+	cin >> n;
+	int m = n;
+	string s = to_string(n);
+	//s << n;
+	for (int i = 0; i < s.length(); i++) {
+		int k = 0;
+		k = m %10;
+		a[k]+=1;
+		m /= 10;
+	}
+	int max = 0;
+	int index = 0;
+	for (int i = 0; i < s.length(); i++) {
+		if (a[i] >= max) {
+			max = a[i];
+			index = i;
+		}	
+	}
+	cout << index;
 
+}
