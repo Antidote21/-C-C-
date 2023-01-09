540bbae0b323973090fa3a3d23b9ab887199528f
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2023-01-09 21:48:19 +0900

    석차 구하기

---
+++ 코드구현력 기르기/25.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main(){
+	//freopen("input.txt", "rt", stdin);
+	int n,i,j;
+	scanf("%d",&n);
+	vector<int> a(n);
+	vector<int> b(n);
+	b.assign(n,1);
+	for(i=0;i<n;i++){
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++){
+		for(j=0;j<n;j++){
+			if(a[j]>a[i]){
+				b[i]++;
+			}
+		}
+	}
+	for(i=0;i<n;i++){
+		printf("%d ",b[i]);
+	}
+
+	return 0;
+}
