63331b2a4091c7e968aaecd36418ccdfa08d911b
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2023-02-15 16:14:51 +0900

    Update 37.c

--- 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/37.c
+++ 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/37.c
@@ -1,4 +1,36 @@
 #include <stdio.h>
+#include <vector>
+using namespace std;
+int main(){
+	freopen("input.txt","rt", stdin);
+	int size, n, i, j, pos, a;
+	scanf("%d %d", &size, &n);
+	vector<int> c(size);
+	for(i=0;i<n; i++){
+		scanf("%d",&a);
+		pos=-1;	
+		for(j=0;j<i;j++){
+			if(c[j]==a)pos=j;
+		}
+		if(pos==-1){
+			for(j=size-1;j>0;j--){
+			c[j]=c[j-1];
+			}
+		}
+		else{
+			for(j=pos;j>0;j--){
+			c[j]=c[j-1];
+			}
+		}
+		c[0]=a;
+	}
+	
+	for(i=0;i<size;i++){
+		printf("%d ", c[i]);
+	}
+	return 0;
+}
+#include <stdio.h>
 int C[20];
 int main(){
 	freopen("input.txt","rt", stdin);
