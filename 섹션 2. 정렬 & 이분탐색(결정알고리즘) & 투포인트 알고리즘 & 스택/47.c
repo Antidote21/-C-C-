5d6f793e8a153fed2e364a5601e74795c98d8bfe
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2023-02-27 23:48:53 +0900

    Update 47.c

--- 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/47.c
+++ 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/47.c
@@ -2,11 +2,11 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+int a[51][51];
 int main(){
 	freopen("input.txt","rt", stdin);
-	int  i, j, cnt=0;
+	int n, i, j, cnt=0;
 	scanf("%d",&n);
-	int a[n+2][n+2]={0,};
 	for(i=0; i<=n+1; i++){
 		for(j=0; j<=n+1; j++){
 			printf("%d",a[i][j]);
@@ -23,6 +23,39 @@ int main(){
 			if((a[i][j]>a[i-1][j])&&(a[i][j]>a[i][j+1])&&(a[i][j]>a[i+1][j])&&(a[i][j]>a[i][j-1])){
 				cnt++;
 			}
+		}
+	}
+	printf("%d",cnt);
+	return 0;
+}
+
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int a[51][51];
+int dx[4]={-1, 0, 1, 0};
+int dy[4]={0, 1, 0, -1};
+int main(){
+	freopen("input.txt","rt", stdin);
+	int  n, i, j, k, cnt=0, flag;
+	scanf("%d", &n);
+	for(i=1; i<=n; i++){
+		for(j=1; j<=n; j++){
+			scanf("%d", &a[i][j]);	
+		}
+	}
+	
+	for(i=1; i<=n; i++){
+		for(j=1; j<=n; j++){
+			flag=0;
+			for(k=0; k<4; k++){
+				if(a[i][j]<=a[i+dx[k]][j+dy[k]]){
+					flag=1;
+					break;
+				}
+			}
+			if(flag==0)cnt++;
 		}
 	}
 	printf("%d",cnt);
