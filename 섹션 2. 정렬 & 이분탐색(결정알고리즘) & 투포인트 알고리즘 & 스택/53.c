88b628d0983cfb075b8e8d33d01a8a6fea8e9189
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2023-03-14 00:50:10 +0900

    Update 53.c

--- 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/53.c
+++ 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/53.c
@@ -26,3 +26,35 @@ int main(){
 	}
 	return 0;
 }
+
+//위 코드의 문제점 16진수 출력 불가 
+
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int stack[100];
+int top=-1;
+
+void push(int x){
+	stack[++top]=x;
+}
+
+int pop(){
+	return stack[top--];
+}
+
+int main(){
+	freopen("input.txt","rt", stdin);
+	int n,k;
+	char str[20]="0123456789ABCDEF";
+	scanf("%d %d",&n,&k);
+	while(n>0){
+		push(n%k);
+		n=n/k;
+	}
+	while(top!=-1){
+		printf("%c",str[pop()]);
+	}
+	return 0;
+}
