65a205d9e058cc6caa331756f755421c92ef3cbf
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2023-03-17 23:34:02 +0900

    Update 51.c

--- 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/51.c
+++ 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/51.c
@@ -1,3 +1,4 @@
+//2차원 배열의 합을 응용하는 문제이다.
 #include <stdio.h>
 #include <vector>
 #include <algorithm>
@@ -10,18 +11,18 @@ int main(){
 	for(i=1;i<=h;i++){
 		for(j=1;j<=w;j++){
 			scanf("%d", &a[i][j]);
-			dy[i][j] = dy[i][j-1]+dy[i-1][j]-dy[i-1][j-1]+a[i][j];
+			dy[i][j] = dy[i][j-1]+dy[i-1][j]-dy[i-1][j-1]+a[i][j];//이 부분이 사실상 이 문제의 핵심이다.
+			//2차원 배열의 값을 더하고 빼는 과정이 익숙해질 수 있도록 하는 문제이다. 
 		}	
 	}
 	
 	scanf("%d %d", &n, &m);
 	for(i=n;i<=h;i++){
 		for(j=m;j<=w;j++)		
-			tmp=dy[i][j]-dy[i-n][j]-dy[i][j-m]+dy[i-n][j-m];
+			tmp=dy[i][j]-dy[i-n][j]-dy[i][j-m]+dy[i-n][j-m];//2차원 배열에서 특정 부분만 더하고 빼는 방법을 알 수 있다. 
 			if(tmp>max) max=tmp;
 		}		
 	}	
 	printf("%d\n", max);
 	return 0;
 }
-
