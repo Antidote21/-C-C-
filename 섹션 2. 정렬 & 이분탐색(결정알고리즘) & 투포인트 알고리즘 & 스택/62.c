b2b2ccd78ac2216ece1bbd113538971219d4ffe6
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2023-03-30 23:59:48 +0900

    Update 62.c

--- 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/62.c
+++ 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/62.c
@@ -2,25 +2,25 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
+//나눌 수 있는 작은 범위까지 나눈다음 다시 병합하여 정렬하는 방법이다.  
 int a[101], tmp[101];
-void divide(int lt, int rt){
+void divide(int lt, int rt){//시작값과 끝값이 매개변수로 들어간다.  
 	int mid;
-	int p1, p2, p3;
 	if(lt<rt){
-		mid=(lt+rt)/2;
+		mid=(lt+rt)/2;//분할시키기 위한 mid 변수  
 		divide(lt, mid);
-		divide(mid+1, rt);
+		divide(mid+1, rt); 
 		p1=lt;
 		p2=mid+1;
 		p3=lt;
-		while(p1<=mid && p2<=rt){
-			if(a[p1]<a[p2]) tmp[p3++]=a[p1++];
-			else tmp[p3++]=a[p2++];
+		while(p1<=mid && p2<=rt){//정렬 하기 위한 while문  
+			if(data[p1]<data[p2])tmp[p3++]=data[p1++];
+			else tmp[p3++]=data[p2++];
 		}
-		while(p1<=mid) tmp[p3++]=a[p1++];
-		while(p2<=rt) tmp[p3++]=a[p2++];
+		while(p1<=mid) tmp[p3++]=data[p1++];
+		while(p2<=rt) tmp[p3++]=data[p2++];
 		for(int i=lt; i<=rt; i++){
-			a[i]=tmp[i];
+			data[i]=tmp[i];//정렬된 값을 data 배열에 대입 
 		}
 	}
 }
