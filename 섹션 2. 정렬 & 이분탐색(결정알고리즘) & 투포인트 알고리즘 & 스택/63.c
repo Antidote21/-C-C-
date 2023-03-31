35219e46a88364ca7a866575f1b9370bdf5d15e7
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2023-03-31 23:49:22 +0900

    인접행렬 C

---
+++ 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/63.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<vector>
+#include<algorithm>
+using namespace std;  
+int a[20][20];//인접행렬 
+int main() {
+	freopen("input.txt", "rt", stdin);
+	int i, j, n, m, p,q;//정점 개수 n 간선 개수  
+	scanf("%d %d",&n,&m); //정점 개수와 간선 개수를 읽는다.  
+	for(i=1;i<=m;i++){//간선의 개수만큼 반복  
+		scanf("%d %d",&p,&q); //scanf("%d %d %d", &p, &q, &a[i][j]); 불가  
+		scanf("%d",&a[p][q]);		
+	}
+	
+	for(i=1;i<=n;i++){
+		for(j=1;j<=n;j++){
+			printf("%d ",a[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
+#include<stdio.h>
+#include<vector>
+#include<algorithm>
+using namespace std;   
+int map[51][51]; 
+int main() {
+	freopen("input.txt", "rt", stdin);
+	int n, m, i, j, a, b, c;
+	scanf("%d %d", &n, &m);
+	for(i=1;i<=m;i++){
+		scanf("%d %d %d", &a, &b, &c);
+		map[a][b]=c;//방향 그래프 map[a][b]=1 
+	} 				//무방향 그래프	map[a][b]=1 map[b][a]=1 
+	for(i=1;i<=n;i++){
+		for(j=1;j<=n;j++){
+			printf("%d ",map[i][j]);
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
