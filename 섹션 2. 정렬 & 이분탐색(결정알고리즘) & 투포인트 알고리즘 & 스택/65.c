d85dbbef5812b181655df65e8217fc4c7e725d32
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2023-04-02 23:56:48 +0900

    미로탐색 R

---
+++ 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/65.c
@@ -0,0 +1,80 @@
+//이 문제를 풀지 못한 이유 
+//상하좌우를 어떻게 탐색할지 구상하지 못했다. 
+ 
+#include<stdio.h>
+#include<vector>
+#include<algorithm>
+using namespace std;  
+int map[8][8], ch[8][8], cnt=0; //ch[] 방문했던 곳 
+
+void DFS(int v, int w){
+	int i;
+	if(v==7 && w==7)cnt++;
+	else{
+		for(i=1; i<=7; i++){
+			if(map[v][w]==0 && ch[v][w]==0){
+				ch[v][i]=1;
+				DFS(v,w++);
+				ch[v][i]=0;
+				DFS(v++,w);
+				ch[v][i]=0;
+			}
+		}
+	}
+	
+} 
+
+int main() {
+	freopen("input.txt", "rt", stdin);
+	int i, j, n;
+	for(i=1; i<=7; i++){
+		for(j=1;j<=7; j++){
+			scanf("%d", &map[i][j]);
+		}
+	}
+	ch[1][1]=1;
+	DFS(1, 1);
+	printf("%d",cnt);
+	return 0;
+}
+
+#include<stdio.h>
+#include<vector>
+#include<algorithm>
+using namespace std;  
+int map[8][8], ch[8][8], cnt=0; //ch[] 방문했던 곳 
+int dx[4]={-1,0,1,0}; 
+int dy[4]={0,1,0,-1}; //상하좌우를 가기위한 배열  
+
+void DFS(int x, int y){
+	int i, xx, yy;//xx, yy는 다음에 갈 좌표  
+	if(x==7 && y==7){ //결국 목적지는 (7,7) 
+		cnt++;
+	}else{
+		for(i=0; i<4; i++){
+			xx=x+dx[i];//좌우 
+			yy=y+dy[i];//상하 
+			if(xx<1 || xx>7 || yy<1 || yy>7)continue;//격자판을 벗어난 좌표는 건너뛴다.  
+			if(map[xx][yy]==0 && ch[xx][yy]==0){//통로인 곳과 아직 가지 않은 곳  
+				ch[xx][yy]=1;
+				DFS(xx,yy);
+				ch[xx][yy]=0;
+			}
+		}
+	}
+}
+
+
+int main() {
+	freopen("input.txt", "rt", stdin);
+  	int i, j;
+	for(i=1; i<=7; i++){
+		for(j=1;j<=7; j++){
+			scanf("%d", &map[i][j]);
+		}
+	} 
+	ch[1][1]=1;
+	DFS(1,1);
+	printf("%d\n", cnt);
+	return 0;
+}
