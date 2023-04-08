7b835fcdc3135ca5a8b1d38f9d81b7d915d3278a
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2023-04-08 23:47:54 +0900

    경로탐색 C

--- 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/64.c
+++ 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/64.c
@@ -31,38 +31,40 @@ int main() {
 }
 
 
+
 #include<stdio.h>
 #include<vector>
 #include<algorithm>
 using namespace std;  
-int map[30][30], ch[30], cnt=0, n; //ch[] 방문했던 곳 
-
+int map[21][21];
+int n, cnt=0;//DFS 함수에서 사용해야 하므로 전역변수로 선언  
+int ch[30];//방문을 했는지 확인하는 배열  
 void DFS(int v){
 	int i;
-	if(v==n){
+	if(v==n){//원하는 정점 n  
 		cnt++;
 	}else{
-		for(i=1; i<=n; i++){
-			if(map[v][i]==1 && ch[i]==0){ //그곳이 갈 수 있고, 아직 가지 않은 곳인지 확인 
+		for(i=1;i<=n;i++){//다른 정점으로 갈 수 있는지를 판단하기 위한 for문 
+			if(map[v][i]==1&&ch[i]==0){//인접행렬에서 연결되어 있고 아직 방문하지 않은 경우  
 				ch[i]=1;
-				DFS(i);
-				ch[i]=0;//마지막에 5를 0으로 바꿔줘야 다시 5로 갈 수 있다. 
+				DFS(i);//v가 아닌 i인 경우에 대해 아직 확실하게 이해하지 못함
+				ch[i]=0;
 			}
 		}
-		
-	}	
-} 
-
+	}
+}  
 int main() {
 	freopen("input.txt", "rt", stdin);
-	int n, m, i, a, b;
+	int  m, i, a, b;
 	scanf("%d %d", &n, &m);
 	for(i=1; i<=m; i++){
 		scanf("%d %d", &a, &b);
-		map[a][b]=1;
+		map[a][b]=1;//인접행렬에서 연결됨을 나타내기 위해 1을 대입
 	}
-	ch[1]=1;
+	ch[1]=1;//처음 시작은 1부터 방문하므로 
 	DFS(1);
-	printf("%d\n", cnt);
+	printf("%d", cnt);
 	return 0;
 }
+
+
