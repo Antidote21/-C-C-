7f22a7329ac294d3b27feeb2cf25f027f101ae03
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2023-04-11 23:20:15 +0900

    경로탐색 R2

--- 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/66.c
+++ 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/66.c
@@ -4,35 +4,35 @@
 #include<vector>
 #include<algorithm>
 using namespace std;  
-int ch[30], cnt=0, n;
-vector<int> map[30];
-
+vector <int> map[20];
+int ch[20], cnt=0, n;
 void DFS(int v){
 	int i;
 	if(v==n){
 		cnt++;
-	}
-	else{
+	}else{		
 		for(i=0; i<map[v].size(); i++){
 			if(ch[map[v][i]]==0){
 				ch[map[v][i]]=1;
 				DFS(map[v][i]);
 				ch[map[v][i]]=0;
-			}
-		}
+			}		
+		}	
 	}
-}
-
+}  
 int main() {
 	freopen("input.txt", "rt", stdin);
-	int m, i, a, b;
-	scanf("%d %d",&n, &m);
+	int i, j, m, a, b;
+	scanf("%d %d", &n, &m);
 	for(i=1; i<=m; i++){
-		scanf("%d %d", &a, &b);
+		scanf("%d %d",&a, &b);
 		map[a].push_back(b);
 	}
 	ch[1]=1;
 	DFS(1);
-	printf("%d\n", cnt); 
+	printf("%d\n",cnt);
 	return 0;
 }
+
+
+
