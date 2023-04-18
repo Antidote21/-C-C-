f8c2397154f71f0a0e460184ff71bef58cfcef16
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2023-04-18 22:21:53 +0900

    최소힙 R

---
+++ 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/74.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<vector> 
+#include<queue>
+#include<algorithm>
+using namespace std;  
+int main() {
+	freopen("input.txt", "rt", stdin);
+	int a;
+	priority_queue<int> pQ;
+	while(true){
+		scanf("%d", &a);
+		if(a==-1)break;
+		if(a==0){
+			if(pQ.empty())printf("-1\n");
+			else {
+				printf("%d\n",-pQ.top()); //큐에서 우선순위가 가장 높은 원소 출력  
+				pQ.pop();
+			}
+		}
+		else{
+			pQ.push(-a);//최소힙으로 만드는 방법	
+		}
+	}
+	return 0;
+}
