1b401118d001a4085ead1862f8ebd4e71a2cca73
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2023-05-09 22:31:34 +0900

    원더랜드 R

---
+++ 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/79.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<algorithm>
+#include<queue>
+#include<vector>
+using namespace std;
+int ch[100];
+struct Edge{
+	int e, val;
+	Edge(int a, int b){
+		e = a;//간선의 목적지 
+		val = b;//간선의 가중치  
+	}
+	bool operator < (const Edge &b) const {//연산자 오버로딩 함수  
+		return val>b.val;//최소힙 -> 가중치 값이 작은 간선 먼저 처리 
+		//return val>b.val; 최대힙 -> 가중치 값이 큰 간선 먼저 처리
+	}
+}
+
+int main(){
+	freopen("input.txt", "rt", stdin);
+	int v,e,i,a,b,c,res=0;
+	priority_queue <Edge> Q;
+	vector<pair<int, int> > map[30];
+	scanf("%d %d", &v, &e);
+	for(i=1; i<=e; i++){
+		scanf("%d %d %d",&a, &b, &c);
+		map[a].push_back(make_pair(b,c)); //a->b무방향 가중치 인접리스트 
+		map[b].push_back(make_pair(a,c)); //b->a무방향 가중치 인접리스트 
+	}
+	Q.push(Edge(1,0));//시작 지점  
+	while(!Q.empty()){
+		Edge tmp=Q.top();
+		Q.pop();
+		int v=tmp.e;
+		int cost=tmp.val;
+		if(ch[v]==0){
+			res+=cost;
+			ch[v]=1;
+			for(i=0; i<map[v].size(); i++){
+				Q.push(Edge(map[v][i].first, map[v][i].second));
+			}
+		}
+	} 
+	printf("%d", res);
+	return 0;
+}
