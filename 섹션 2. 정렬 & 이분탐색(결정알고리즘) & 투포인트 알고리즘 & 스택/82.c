434d2140be1b68bd21616b88a48a7b673b325da4
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2023-05-16 23:18:26 +0900

    순열 구하기 C

--- 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/82.c
+++ 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/82.c
@@ -3,36 +3,38 @@
 #include<queue>
 #include<vector>
 using namespace std;
-int n, r, cnt=0;
-int a[20], res[20], ch[20];// 선택된 원소는 다시 선택되지 않도록 하는 ch배열  
-int DFS(int L){
-	int i,j;
-	if(L==r){ 
-		for(j=0; j<L; j++){
-			printf("%d ", res[j]);
+int n, r, cnt=0;  
+int ch[20], res[20], a[20];
+int DFS(int L){//L은 현재까지 만들어진 순열 길이  
+	int i, j;
+	if(L==r){//r길이의 순열이 완성
+		for(j=0; j<r; j++){
+			printf("%d ",res[j]);//결과 출력  
 		}
 		cnt++;
-		puts("");
+		puts("");//빈 줄 출력  
 	}
 	else{
-		for(i=1;i<=n;i++){
-			if(ch[i]==0){
+		for(i=1; i<=n; i++){//아직 사용되지 않은 숫자를 찾는다. 
+			if(ch[i]==0){//각 숫자의 사용 여부 파악 
 				res[L]=a[i];
-				ch[i]=1;
-				DFS(L+1);
-				ch[i]=0;
+				ch[i]=1;//숫자 i가 사용
+				DFS(L+1);//재귀 호출을 통해 모든 가능한 순열을 생성
+				ch[i]=0;//재귀 호출이 완료되면, ch[i]를 다시 0으로 설정하여 숫자 i를 다른 순열에서 사용
+				//백트래킹(Backtracking) 해결해야 할 문제의 가능한 모든 경우의 수를 탐색하면서 해를 찾는 기법
 			}
 		}
 	}
 }
+
 int main(){
 	freopen("input.txt", "rt", stdin);	
-	int i, k;
-	scanf("%d %d",&n, &r);
-	for(i=1;i<=n; i++){
-		scanf("%d", &a[i]);
+	scanf("%d %d", &n, &r); 
+	int i;
+	for(i=1; i<=n; i++){
+		scanf("%d",&a[i]);
 	}
 	DFS(0);
-	printf("%d",cnt);
+	printf("%d", cnt);
 	return 0;
 }
