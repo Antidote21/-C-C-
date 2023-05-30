a37dbb2c8ba003195f27fc14feb3d641ea9c5d71
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2023-05-30 23:58:45 +0900

    자료구조 지식

---
+++ 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/map.c
@@ -0,0 +1,33 @@
+#include<fstream>
+#include<iostream>
+#include<map>
+#include <string>
+using namespace std;
+
+int main(){
+	freopen("input.txt", "rt", stdin);
+	ios_base::sync_with_stdio(false);
+	ifstream cin;
+	cin.open("input.txt");
+	map<string, int> ch;
+	map<string, int>::iterator it;
+	char a[100];
+	int n;
+	cin >> n;
+	for(int i=1; i<=n; i++){
+		cin>> a;
+		ch[a]++;
+	}
+	
+	int max=0;
+	string res;
+	for(it=ch.begin(); it != ch.end(); it++ ){
+		if(it->second>max){
+			max = it->second;
+			res=it->first;
+		}
+	}
+	cout<< res << " : " <<max<<"\n";
+	
+	return 0;
+}
