fd24cf06a63cca8d5cc85dbedb1dfe8856ea8328
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2023-07-05 21:01:36 +0900

    냅색 알고리즘 C

--- 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/9.cpp
+++ 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/9.cpp
@@ -1,7 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
+//dy[i]테이블의 의미 = 가방에 j무게를 담을 때,  보석의 최대 가치  
 int main(){
-	ios_base::sync_with_stdio(false);
+	ios_base::sync_with_stdio(false); 
 	freopen("input.txt", "rt", stdin);
 	int n, m, w, v;
 	cin>>n>>m;
@@ -9,9 +10,10 @@ int main(){
 	for(int i=0; i<n; i++){
 		cin>>w>>v;
 		for(int j=w; j<=m; j++){
-			dy[j]=max(dy[j], dy[j-w]+v);
+			dy[j]=max(dy[j], dy[j-w]+v);//기존값과 w라는 무게를 가방에 담는다는 가정 
 		}
 	}
+	
 	cout<<dy[m];
 	return 0;
 }
