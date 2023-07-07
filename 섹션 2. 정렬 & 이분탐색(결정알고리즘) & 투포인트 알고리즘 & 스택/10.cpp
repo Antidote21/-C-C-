5a6fdf386ce91c391ffe17207dd5443337fb54e5
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2023-07-07 22:00:41 +0900

    동전교환 C

--- 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/10.cpp
+++ 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/10.cpp
@@ -3,16 +3,16 @@ using namespace std;
 int main(){
 	ios_base::sync_with_stdio(false);
 	freopen("input.txt", "rt", stdin);
-	int n, m;
+	int n, m;//n 동전의 종류 수, m 거스름돈의 금액
 	cin>>n;
-	vector<int> coin(n);
+	vector<int> coin(n);//동전의 종류를 저장하는 벡터
 	for(int i=0; i<n; i++) cin>>coin[i];
 	cin>>m;
-	vector<int> dy(m+1, 1000);
-	dy[0]=0;
-	for(int i=0; i<n; i++){
-		for(int j=coin[i]; j<=m; j++){
-			dy[j]=min(dy[j], dy[j-coin[i]]+1);
+	vector<int> dy(m+1, 1000);//거스름돈의 최소 동전 개수를 저장하는 벡터
+	dy[0]=0;//거스름돈이 0일 때의 최소 동전 개수
+	for(int i=0; i<n; i++){//동전의 종류를 순회
+		for(int j=coin[i]; j<=m; j++){//거스름돈을 순회
+			dy[j]=min(dy[j], dy[j-coin[i]]+1);//dy[j-coin[i]]+1 현재 동전을 사용해서 거스름돈을 만들기 위해 필요한 동전 개수
 		}
 	}
 	cout<<dy[m];
