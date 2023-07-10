3025feea776413774042f79621ee700f1e6e2572
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2023-07-10 23:59:19 +0900

    플루이드 와샬 R

---
+++ 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/12.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	ios_base::sync_with_stdio(false);
+	freopen("input.txt", "rt", stdin);	
+	int n, m, a, b, c;
+	cin>>n>>m; //n은 정점의 개수 m은 간선의 개수  
+	vector<vector<int> > dis(n+1, vector<int>(n+1, 5000)); //다이나믹 테이블 2차원 
+	for(int i=1; i<=n; i++) dis[i][i]=0;//자기자신으로 가는 것(대각선)을 0으로 초기화  
+	for(int i=1; i<=m; i++){
+		cin>>a>>b>>c;
+		dis[a][b]=c;//중간 노드를 거치지 않고 바로간 경우 	
+	}
+	//플루이드 와샬 알고리즘  
+	for(int k=1; k<=n; k++){
+		for(int i=1; i<=n; i++){
+			for(int j=1; j<=n; j++){
+				if(dis[i][j]>dis[i][k]+dis[k][j]){//i -> k -> j 가는 과정  
+					dis[i][j]=dis[i][k]+dis[k][j];
+				}
+			}
+		}
+	}
+	for(int i=1; i<=n; i++){
+		for(int j=1; j<=n; j++){
+			if(dis[i][j]==5000){
+				cout<<"M ";
+			}
+			else cout<<dis[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+
+	return 0;
+}
