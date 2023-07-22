f038186c41c6fee12ebdc81210eb58e71507c0a7
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2023-07-22 23:51:48 +0900

    최소비용 C

--- 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/68.c
+++ 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/68.c
@@ -42,6 +42,42 @@ int main() {
 	return 0;
 }
 
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int ch[30], n, cost=2147000000;
+vector<pair<int, int> > map[30];
+
+void DFS(int v, int sum){
+	if(v==n){
+		if(cost>sum)cost=sum;
+	}else{
+		 for(int i=0; i<map[v].size(); i++){
+		 	if(ch[map[v][i].first]==0){
+		 		ch[map[v][i].first]=1;
+		 		DFS(map[v][i].first, sum + map[v][i].second);
+		 		ch[map[v][i].first]=0;
+			 }
+		 }
+	}
+}
+
+int main(){
+	freopen("input.txt", "rt", stdin);
+	int m, a, b, c;
+	cin>>n>>m;
+	for(int i=1; i<=m; i++){
+		cin>>a>>b>>c;
+		map[a].push_back(make_pair(b, c));
+	}
+	ch[1]=1;
+	DFS(1, 0);
+	cout<<cost;
+	return 0;
+}
+
+
 
 
 
