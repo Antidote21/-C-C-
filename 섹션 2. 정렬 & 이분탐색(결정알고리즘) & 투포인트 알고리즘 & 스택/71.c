80d4ede2cc9018086212091ef603d5e78da80ae8
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2023-07-27 22:48:14 +0900

    송아지 찾기 R

--- 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/71.c
+++ 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/71.c
@@ -30,3 +30,40 @@ int main() {
 	}
 	return 0;
 }
+
+
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+using namespace std;
+int ch[10000], d[3]={1, -1, 5};
+int main(){
+	freopen("input.txt", "rt", stdin);
+	int s, e, x, pos;
+	cin>>s>>e;
+	queue<int> Q;
+	Q.push(s);
+	ch[s]=1;
+	while(!Q.empty()){	
+		x = Q.front();
+		Q.pop();
+		for(int i=0; i<3; i++){
+			pos=x+d[i];
+			if(pos<=0 || pos > 10000)continue;
+			if(pos==e){
+				cout<<ch[x];
+				exit(0);
+			}
+			else{
+				if(ch[pos]==0){
+					ch[pos]=ch[x]+1;
+					Q.push(pos);
+				}
+			}
+	
+		}
+	
+	}
+	return 0;
+}
