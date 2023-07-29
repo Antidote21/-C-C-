3b3a956d5f8c0fd55822cfa68c9d944c219b65fa
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2023-07-29 21:31:53 +0900

    휴가 C

--- 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/84.c
+++ 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/84.c
@@ -29,3 +29,33 @@ int main(){
 	printf("%d",res);
 	return 0;
 }
+
+#include<bits/stdc++.h>
+using namespace std;
+int n, res=0;
+vector< vector<int> > a(20);
+void DFS(int L, int sum){
+	if(L>n+1)return;
+	if(L==n+1){
+		res = max(res, sum);
+	}else{
+		DFS(L+a[L][0], sum+a[L][1]);	
+		DFS(L+1, sum);
+	}
+}
+
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	freopen("input.txt", "rt", stdin);
+	int t, p;
+	cin>>n;
+	for(int i=1; i<=n; i++){
+		cin>>t>>p;
+		a[i].push_back(t);
+		a[i].push_back(p);
+	}
+	DFS(1,0);
+	cout<<res;
+	return 0;
+}
