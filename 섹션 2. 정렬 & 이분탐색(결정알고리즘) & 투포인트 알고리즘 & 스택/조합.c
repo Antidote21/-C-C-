69036ef53cbf06668cca558d4dae418d5bd6d351
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2023-07-31 22:41:16 +0900

    조합 R

--- 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/조합.c
+++ 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/조합.c
@@ -25,3 +25,36 @@ int main(){
 	DFS(0, 0);
 	return 0;
 }
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int n, r;
+int ch[20];
+
+void DFS(int s, int L){
+	if(L==r){
+		for(int i=0; i<r; i++){
+			cout<<ch[i]<<" ";
+		}
+		cout<<endl;		
+	}else{
+		for(int i=s; i<n; i++){
+//			if(ch[L]==0){
+//				ch[L]=i;
+//				DFS(i+1, L+1);
+//				ch[L]=0;	
+//			}
+			ch[L]=i;
+			DFS(i+1, L+1);
+		}
+	}
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	freopen("input.txt", "rt", stdin);
+	cin>>n>>r;
+	DFS(0, 0);
+	return 0;
+}
