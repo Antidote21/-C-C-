1559116b0f14d8422ca5fcbbd3ffb92a50c5b3fb
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2023-08-01 22:23:48 +0900

    피자 배달 거리 R

--- 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/86.cpp
+++ 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/86.cpp
@@ -43,3 +43,49 @@ int main(){
 	cout<<res;
 	return 0;
 }
+
+#include<bits/stdc++.h>
+using namespace std;
+int n, m, res=100, dis;
+int a[50][50];
+int ch[30];
+vector<pair<int, int > >home;
+vector<pair<int, int > >pizza;
+void DFS(int s, int L){
+	if(L==m){
+		int sum=0;
+		for(int i=0; i<home.size(); i++){
+			int x1 = home[i].first;
+			int y1 = home[i].second;
+			dis=100;
+			for(int j=0; j<m; j++){
+				int x2 = pizza[ch[j]].first;
+				int y2 = pizza[ch[j]].second;
+				dis = min(dis, abs(x1-x2) + abs(y1-y2));
+			}
+			sum+=dis;//현재 선택한 피자집들과 모든 집들과의 거리의 합			
+		}
+		res = min(res, sum);
+	}else{
+		for(int i=s; i<pizza.size(); i++){
+			ch[L]=i;
+			DFS(i+1, L+1);
+		}
+	}
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	freopen("input.txt", "rt", stdin);
+	cin>>n>>m;
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			cin>>a[i][j];
+			if(a[i][j]==1)home.push_back(make_pair(i,j));
+			if(a[i][j]==2)pizza.push_back(make_pair(i,j));
+		}
+	}
+	DFS(0, 0);
+	cout<<res;
+	return 0;
+}
