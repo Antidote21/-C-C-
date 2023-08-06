e7cce02c90b636990989372003eb4a0f5ebfb31b
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2023-08-07 05:21:58 +0900

    심바 R

--- 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/90.c
+++ 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/90.c
@@ -85,3 +85,86 @@ int main(){
 	printf("%d\n", res);
 	return 0;
 }
+#include<bits/stdc++.h>
+using namespace std;
+int Map[30][30]; 
+int ch[30][30];
+int dx[4] = {0,0,-1,1};
+int dy[4] = {1,-1,0,0};
+queue<pair<int, int> > Q;
+
+struct State{
+	int x, y, dis;
+	State(int a, int b, int c){
+		x = a;
+		y = b;
+		dis = c;
+	}
+	bool operator<(const State &b)const {
+		if(dis != b.dis) return dis > b.dis;
+		if(x != b.x) return x > b.x;
+		else return y > b.y;
+	}
+};
+
+struct Lion{
+	int x, y, s, ate;
+	void sizeUp(){
+		ate = 0;
+		s++;
+	}
+};
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	freopen("input.txt", "rt", stdin);
+	int n, i, j, res=0;
+	priority_queue<State> Q;
+	Lion simba;
+	cin>>n;
+	for(i=1; i<=n; i++){
+		for(j=1; j<=n; j++){
+			cin>>Map[i][j];
+			if(Map[i][j]==9){
+				simba.x=i;
+				simba.y=j;
+				Map[i][j]=0;
+			}
+		}
+	}
+	Q.push(State(simba.x, simba.y, 0));
+	simba.s=2;
+	simba.ate=0;
+	while(!Q.empty()){
+		State tmp = Q.top();
+		Q.pop();
+		int x = tmp.x;
+		int y = tmp.y;
+		int z = tmp.dis;
+		if(Map[x][y]!=0 && Map[x][y] < simba.s){
+			simba.x = x;
+			simba.y = y;
+			simba.ate++;
+			if(simba.ate>=simba.s) simba.sizeUp();
+			Map[x][y] = 0;
+			for(i=1; i<=n; i++){
+				for(j=1; j<=n; j++){
+					ch[i][j]=0;
+				}
+			}
+			while(!Q.empty())Q.pop();
+			res=tmp.dis;
+		}
+		for(i=0; i<4; i++){
+			int xx = x + dx[i];
+			int yy = y + dy[i];
+			if(xx<1 || xx>n || yy<1 || yy>n || Map[xx][yy]>simba.s || ch[xx][yy]>0) continue;
+			Q.push(State(xx, yy, z+1));
+			ch[xx][yy] = 1;
+		}
+	}
+	cout<<res;
+	
+	return 0;
+}
+
