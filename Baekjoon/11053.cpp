79664538eac2225ca791cff85086adb79fb04a55
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2023-09-13 23:21:08 +0900

    가장 긴 증가하는 부분 수열

---
+++ Baekjoon/11053.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    vector<int> dp(n, 1);
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            if (arr[i] > arr[j] && dp[i] < dp[j] + 1) {
+                dp[i] = dp[j] + 1;
+            }
+        }
+    }
+
+    int res = 0;
+    for (int i = 0; i < n; i++) {
+        res = max(res, dp[i]);
+    }
+    
+    cout << res;
+
+    return 0;
+}
