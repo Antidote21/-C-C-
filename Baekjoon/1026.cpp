dba360fea57f689089e1a9390e550615b123dba8
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2023-12-14 19:20:12 +0900

    보물

--- Baekjoon/1026.cpp
+++ Baekjoon/1026.cpp
@@ -38,3 +38,29 @@ int main() {
  
     return 0;
 }
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    vector<int> b(n);
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+    }
+    for(int i=0; i<n; i++){
+        cin>>b[i];
+    }
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    
+    int res=0;
+    for(int i=0; i<n; i++){
+        res += a[i]*b[n-1-i];
+    }
+    cout<<res;
+}
