3ce3a742e5ec5d8c75175574c4f09cb36426c32a
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2023-12-25 22:25:09 +0900

    알고리즘 수업 - 깊이 우선 탐색 1 R

--- Baekjoon/24479.cpp
+++ Baekjoon/24479.cpp
@@ -37,3 +37,44 @@ int main(){
     dfs(r);
     return 0;
 }
+//방문여부를 기준으로 정점 순서 증가를 고려하지 않음, 인접 정점이 오름차순이라는 제한 사항을 고려하지 않음, cin과 cout 사용시 시간 초과 
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+vector<int> graph[100001];
+int visited[100001] = { 0, };
+int result[100001];
+int cnt = 0;
+
+void dfs(int r) {
+	if (visited[r] == 1) {
+		return;
+	}
+
+	visited[r] = 1; 
+	cnt++;
+	result[r] = cnt;
+
+	for (int i = 0; i < graph[r].size(); i++) {
+		dfs(graph[r][i]); 
+	}
+}
+int main() {
+	int n, m, r;
+	scanf("%d %d %d", &n, &m, &r);
+	for (int i = 1; i <= m; i++) {
+		int a, b;
+		scanf("%d %d", &a, &b);
+		graph[a].push_back(b); 
+		graph[b].push_back(a); 
+	}
+	for (int i = 1; i <= n; i++) {
+		sort(graph[i].begin(), graph[i].end());
+	}
+	dfs(r);
+	for (int i = 1; i <= n; i++) {
+		printf("%d\n", result[i]);
+	}
+	
+}
