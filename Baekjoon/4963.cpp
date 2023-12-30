00bacd4d572e8cbbaef89d65e1699a4304b2b3de
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2023-12-30 22:57:37 +0900

    섬의 개수 R

--- Baekjoon/4963.cpp
+++ Baekjoon/4963.cpp
@@ -1,42 +1,44 @@
+
 #include<iostream>
+#include<string.h> 
 using namespace std;
 int n, m, cnt;
 int A[51][51];
-int dx[4]={0,0,-1,1};
-int dy[4]={1,-1,0,0};
-void dfs(int x, int y){
-    if(x>n||y>m){
-        cout<<cnt<<endl;
-        return;
-    }
-    else{
-        // int w,h;
-        for(int i=0; i<4; i++){
-            for(int j=0; j<4; j++){
-                // cin>>w>>h;
-                if(x+dx[i]<n && x+dx[i]>0 && y+dy[j]<m && y+dy[j]>0){
-                    if(A[x+dx[i]][y+dy[j]]==1){
-                        cnt++;
-                        dfs(x+dx[i], y+dy[j]);
-                    }
-                }
-              
-            }
+int ch[51][51];
+//4방향이 아닌 8방향으로 이동 변경 
+int dx[8]={1,1,0,-1,-1,-1,0,1},dy[8]={0,1,1,1,0,-1,-1,-1};
+void dfs(int y, int x){
+    // dfs(y,x)
+    //이중 for문이 아닌 for문으로 이동
+    for(int i=0; i<8; i++){
+        if(x+dx[i]<n && x+dx[i]>=0 && y+dy[i]<m && y+dy[i]>=0 && ch[y+dy[i]][x+dx[i]]==0&&A[y+dy[i]][x+dx[i]]==1){
+            ch[y+dy[i]][x+dx[i]]=1;
+            dfs(y+dy[i],x+dx[i]);
         }
-
     }
+
 }
 int main(){
     while(true){
+        int cnt=0;
         cin>>n>>m;
+        //입력 첫번째 for문 m 두번째 for문 n 으로 변경
         if(n==0&&m==0)break;
-        else{
-            for(int i=1; i<=n; i++){
-                for(int j=1; j<=m; j++){
-                    cin>>A[i][j];
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                cin>>A[i][j];
+            }
+        }
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(!ch[i][j]&&A[i][j]){
+                    ch[i][j]=1;
+                    dfs(i,j);
+                    cnt++;
                 }
             }
-            dfs(1, 1);
         }
+        cout<<cnt<<"\n";
+        memset(ch,0,sizeof(ch));//memset으로 ch 초기화(헤더 추가)
     }
 }
