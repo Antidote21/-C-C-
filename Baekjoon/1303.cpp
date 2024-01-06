cb075d4d22d72371370761338bf5ce995aeab2a3
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-01-06 16:55:10 +0900

    전쟁 - 전투 R

--- Baekjoon/1303.cpp
+++ Baekjoon/1303.cpp
@@ -3,45 +3,51 @@
 #include<vector>
 using namespace std;
 char map[101][101];
-int ch1[101][101];
-int ch2[101][101];
-int dx[4] = {0,0,-1,1};
-int dy[4] = {1,-1,0,0};
+int ch[101][101];
+int dx[4] = {0, 0, -1, 1};
+int dy[4] = {1, -1, 0, 0};
+int blue, white, n, m;  
 queue<pair<int, int>> Q;
-void bfs(int x, int y){
+void bfs(int x, int y, char c){
+    Q.push({x, y});
+    ch[x][y] = 1;
     
-}
-int main(){
-    int n, m, cnt=0;
-    cin>>n>>m;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            scanf("%c",&map[i][j]);
-        }
-    }
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            if(map[i][j]=='W'&&ch1[i][j]==0){
-                Q.push(make_pair(i,j));
-                ch1[i][j]=1;
-                while(!Q.empty()){
-                    pair<int, int> pos = Q.front();
-                    Q.pop();
-                    for(int i=0; i<4; i++){
-                        int xx=pos.first+dx[i];
-                        int yy=pos.second+dy[i];
-                        if(xx>=0 && xx<n && yy>=0 && yy<m &&map[xx][yy]=='W'){
-                            Q.push(make_pair(xx,yy));
-                            ch1[xx][yy]=1;
-                            cnt++;
-                        }
-                    }
-                }
-                cnt++;
+    int cnt = 0;
+    while(!Q.empty()){
+        pair<int, int> now = Q.front();
+        Q.pop();
+        cnt++;
+        
+        for(int i = 0; i < 4; i++){
+            int xx = now.first + dx[i];
+            int yy = now.second + dy[i];
+            if(xx >= 0 && xx < n && yy >= 0 && yy < m && ch[xx][yy] != 1 && map[xx][yy] == c){
+                ch[xx][yy] = 1;
+                Q.push({xx, yy});
             }
         }
     }
-    
-    cout<<cnt<<endl;
+    if(c == 'W'){
+        white += cnt * cnt;
+    } else {
+        blue += cnt * cnt;
+    }
+}
+int main(){
+    cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cin >> map[i][j];  
+        }
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            if(ch[i][j] != 1){
+                bfs(i, j, map[i][j]);
+            }
+        }
+    }
+
+    cout << white << ' ' << blue;
     return 0;
 }
