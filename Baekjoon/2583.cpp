1ed23366d674bee9eee174b73d86aa79cb0e4590
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-01-09 14:19:57 +0900

    영역 구하기 R

--- Baekjoon/2583.cpp
+++ Baekjoon/2583.cpp
@@ -1,45 +1,54 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
-int m, n, k, cnt;
+int m, n, k, cnt, total_n;
 int map[101][101];
 int visited[101][101];
 int dx[4]={0,0,-1,1};
 int dy[4]={1,-1,0,0};
 vector<int> res;
+
 void dfs(int x, int y){
- 
-    if(visited[x][y]==0){
-        visited[x][y]=1;
-        cnt=0;
-        for(int i=0; i<4; i++){
-            int xx, yy;
-            xx=x+dx[i];
-            yy=y+dy[i];
-            if(xx>n&&xx<0&&yy<0&&yy>m)continue;
-            if(visited[xx][yy]==0&&map[xx][yy]==0){
-                cnt++;
-                dfs(xx,yy);
-            }
+    for(int i=0; i<4; i++){
+        int xx, yy;
+        xx = x + dx[i];
+        yy = y + dy[i];
+        if(xx >= 0 && xx < n && yy >= 0 && yy < m && visited[yy][xx] == 0 && map[yy][xx] == 0){
+            cnt += 1;
+            visited[yy][xx] = 1;
+            dfs(xx, yy);
         }
-        res.push_back(cnt);
-        
     }
 }
+
 int main(){
-    cin>>m>>n>>k;
+    cin >> m >> n >> k;
     for(int i=0; i<k; i++){
-        int x1,x2,y1,y2;
-        cin>>x1>>y1>>x2>>y2;      
-        for(int j=y1; j<=y2; j++){
-            for(int h=x1; h<=x2; h++){
+        int x1, x2, y1, y2;
+        cin >> x1 >> y1 >> x2 >> y2;      
+        for(int j=y1; j<y2; j++){
+            for(int h=x1; h<x2; h++){
                 map[j][h] = 1;
             }
         }
     }
-    dfs(0,0);
-    for(int i=0; i<res.size(); i++){
-        cout<<res[i]<<' ';
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            if(map[i][j] == 0 && visited[i][j] == 0){
+                visited[i][j] = 1;
+                cnt = 1;
+                dfs(j, i); 
+                res.push_back(cnt);
+                total_n++;
+            }
+        }
     }
+    cout << total_n << '\n';
+    sort(res.begin(), res.end());
+    for(int i=0; i<res.size(); i++){
+        cout << res[i] << ' ';
+    }
+    return 0;
 }
