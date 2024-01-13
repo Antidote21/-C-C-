bcd1bec97c6d3fda341f42479b70a9018a7960df
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-01-13 23:20:30 +0900

    프로그래머스 R

--- 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/H-Index.cpp
+++ 섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/H-Index.cpp
@@ -1,42 +1,22 @@
-#include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
-vector<int> citations  = {3, 0, 6, 1, 5};
 
-int binarySearch(int A[], int low, int high, int target){
-    while(low <= high){
-        int mid = (low + high) / 2;
-        if(A[mid] == target) return mid;
-        if(A[mid] > target) high = mid - 1;
-        else low = mid + 1;
-    }
-    return -1;
-}
-
-int solution( vector<int> a) {
+int solution(vector<int> citations) {
     int answer = 0;
-    int k = citations.size();
-    sort(citations.begin(), citations.end());
-    answer = citations[0];
-    int lt=0, rt=k;
+    int n = citations.size();
+    //내림차순 정렬을 하는 이유는 H-index 이상인 값을 구하기 위해서 
+    sort(citations.begin(), citations.end(), greater<int>());
 
-    for(int i=0; i<k; i++){
-        if(citations[i]>=answer){
-            lt++;
-            rt=k-lt;
-            answer=lt;
-        }else if(citations[i]<=answer){
-            if(answer>=lt&&answer>=rt-lt)break;
+    for (int i = 0; i < n; i++) {
+        if (citations[i] >= i + 1) {//H-index값 안에 있는 경우 
+            answer = i + 1;
+        } else {//H-index값을 초과하는 경우 
+            break;
         }
     }
-    return answer;
-}
 
-int main(){
-    int res;
-    res = solution(citations);
-    cout<<res;
+    return answer;
 }
