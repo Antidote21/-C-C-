a1bdf9105d59a7c92cd1f18110224153d73e4623
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-01-15 22:35:25 +0900

    구명보트 R

--- programmers/구명보트.cpp
+++ programmers/구명보트.cpp
@@ -3,19 +3,20 @@
 #include <algorithm>
 
 using namespace std;
-
+//작은 것부터 채워가는 것이 아니라 가장 작은 요소와 가장 큰 요소를 동시에 고려해야 함 
 int solution(vector<int> people, int limit) {
     int answer = 0;
-    int sum = 0;
-    sort(people.begin(), people.end());
-    for(int i=0; i<people.size(); i++){
-        if(sum+people[i]<=limit){
-            sum+=people[i];
-        }else if(sum+people[i]>limit){
-            sum=people[i];
+    sort(people.begin(),people.end());
+    int idx=0;
+    while(idx<people.size()){
+        int back = people.back();
+        people.pop_back();
+        if(people[idx]+back<=limit){
+            answer++;
+            idx++;
+        }else{
             answer++;
         }
     }
-    answer+=1;
     return answer;
 }
