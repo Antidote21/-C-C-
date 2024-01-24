5aa98172f7afd17b0f8da8964cc4ffb6a2aa609b
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-01-24 21:46:07 +0900

    그리디 R

--- programmers/체육복.cpp
+++ programmers/체육복.cpp
@@ -21,3 +21,42 @@ int solution(int n, vector<int> lost, vector<int> reserve) {
     }
     return answer;
 }
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, vector<int> lost, vector<int> reserve){
+    int answer = 0;
+    
+    vector<int> students(n,1);
+    
+    for(int l : lost){
+        students[l-1]--;
+    }
+    
+    for(int r : reserve){
+        students[r-1]++;
+    }
+    
+    for(int i=0; i<n; i++){
+        if(students[i] == 0){
+            if(i>0 && students[i-1] == 2){
+                students[i]++;
+                students[i-1]--;
+            } else if(i<n-1 && students[i+1]==2){
+                students[i]++;
+                students[i+1]--;
+            }
+        }
+    }
+    
+    for(int s : students){
+        if(s >= 1){
+            answer++;
+        }
+    }
+    
+    return answer;
+}
