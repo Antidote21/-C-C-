bf7f133acba70699573868061bef075e64652c9a
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-02-04 21:28:56 +0900

    스택/큐 R

--- programmers/프로세스.cpp
+++ programmers/프로세스.cpp
@@ -29,3 +29,36 @@ int solution(vector<int> priorities, int location) {
     
     return answer;
 }
+
+#include <string>
+#include <vector>
+#include <queue>
+using namespace std;
+priority_queue<int> pq;
+queue<pair<int, int>> q;
+int solution(vector<int> priorities, int location) {
+    int answer = 0;
+    for(int i=0; i<priorities.size(); i++){
+        q.push({i, priorities[i]});
+        pq.push(priorities[i]);
+    }
+    
+    int cnt=1;
+    while(true){
+        pair<int, int> QFront = q.front();
+        q.pop();
+        if(QFront.second==pq.top()){
+            if(QFront.first==location){
+                answer = cnt;
+                break;
+            }else{
+                pq.pop();
+                cnt++;
+            }  
+        }else if(QFront.second!=pq.top()){
+            q.push({QFront.first, QFront.second});
+        }
+        
+    }
+    return answer;
+}
