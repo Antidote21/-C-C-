88423b40fbe7a431336ead38b173f250ddf091fe
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-02-06 23:22:41 +0900

    스택/큐 R

--- programmers/올바른 괄호.cpp
+++ programmers/올바른 괄호.cpp
@@ -33,3 +33,27 @@ bool solution(string s)
     
     return answer;
 }
+//괄호 개수를 각각 판단하여 사용하는 것만으로는 부족 && 굳이 stack에 push하고 그걸 다시 꺼내면서 검사는 비효율적
+
+#include <string>
+#include <stack>
+
+using namespace std;
+
+bool solution(string s) {
+    stack<char> st;
+    
+    for(int i = 0; i < s.size(); i++) {
+        if(s[i] == '(') {
+            st.push(s[i]);
+        } else if(s[i] == ')') {
+            if(!st.empty()) {
+                st.pop();
+            } else {
+                return false; 
+            }
+        }
+    }
+
+    return st.empty();
+}
