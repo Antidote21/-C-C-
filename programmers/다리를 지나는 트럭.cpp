007deec7ee5daf400cf3f58c22225ba4bc305a67
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-02-08 21:45:23 +0900

    스택/큐 RR

--- programmers/다리를 지나는 트럭.cpp
+++ programmers/다리를 지나는 트럭.cpp
@@ -1,26 +1,35 @@
-#include <string>
-#include <vector>
-#include <queue>
-
-using namespace std;
-
-int solution(int bridge_length, int weight, vector<int> truck_weights) {
-    int answer = 0;
-    int sum=0;
-    int idx=0;
-    queue<int> Q;
-    
-    while(idx!=truck_weights.size()){
-        if(weight>=sum+truck_weights[idx]){
-            sum+=truck_weights[idx];
-            Q.push(truck_weights[idx]);
-            idx+=1;
-        }else{
-            sum-=Q.front();
-            Q.pop();
-            answer += bridge_length;
-        }
-    }
-
-    return answer;
+#include <string>
+#include <vector>
+#include <queue>
+ 
+using namespace std;
+ 
+int solution(int bridge_length, int weight, vector<int> truck_weights) {
+    int answer = 0;
+    int next_truck = 0;     
+    int weight_sum = 0;    
+    queue<int> on_bridge;  
+ 
+    for (int i = 0; i < bridge_length; i++)
+        on_bridge.push(0);
+ 
+    while (!on_bridge.empty()) {
+        answer++;
+        weight_sum -= on_bridge.front();
+        on_bridge.pop();
+ 
+        if (next_truck < truck_weights.size()) {
+            if (weight_sum + truck_weights[next_truck] <= weight) {
+                weight_sum += truck_weights[next_truck];
+                on_bridge.push(truck_weights[next_truck]);
+                next_truck++;
+            }
+            else {
+                on_bridge.push(0);
+            }
+        }
+        
+    }
+ 
+    return answer;
 }
