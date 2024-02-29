996f6c4f2b450b10a4becec47fd85de1da753367
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-02-29 15:45:34 +0900

    해시 R

--- programmers/베스트앨범.cpp
+++ programmers/베스트앨범.cpp
@@ -1,50 +1,46 @@
 #include <string>
 #include <vector>
-#include <unordered_map>
 #include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
-
-struct Song {
-    int index; // 고유 번호
-    int play; // 재생 횟수
+struct Song{
+    int num;
+    int count;
 };
 
-bool cmp(const Song& a, const Song& b) {
-    if (a.play == b.play) {
-        return a.index < b.index;
+bool cmp(const Song& a, const Song& b){
+    if(a.count==b.count){
+        return a.num<b.num;
     }
-    return a.play > b.play;
+    return a.count > b.count;
 }
 
-bool cmp2(const pair<string, int>& a, const pair<string, int>& b) {
+bool cmp2(const pair<string, int> &a, const pair<string, int> & b){
     return a.second > b.second;
 }
 
 vector<int> solution(vector<string> genres, vector<int> plays) {
     vector<int> answer;
-
     unordered_map<string, vector<Song>> genreMap;
-    unordered_map<string, int> playTotal;
-
-    for (int i = 0; i < genres.size(); ++i) {
-        genreMap[genres[i]].push_back({i, plays[i]});
-        playTotal[genres[i]] += plays[i];
+    unordered_map<string, int> totalPlays;  
+    
+    for(int i=0; i<genres.size(); i++){
+        genreMap[genres[i]].push_back({i,plays[i]});
+        totalPlays[genres[i]] +=plays[i];
     }
-
-    vector<pair<string, int>> sortedGenres(playTotal.begin(), playTotal.end());
+    vector<pair<string, int>> sortedGenres(totalPlays.begin(), totalPlays.end());
     sort(sortedGenres.begin(), sortedGenres.end(), cmp2);
-
-    for (const auto& genre : sortedGenres) {
+    
+    for(auto genre : sortedGenres){
         vector<Song>& songs = genreMap[genre.first];
         sort(songs.begin(), songs.end(), cmp);
-        int count = 0;
-        for (const auto& song : songs) {
-            answer.push_back(song.index);
-            if (++count >= 2) break; 
+        int cnt=0;
+        for(auto song: songs){
+            answer.push_back(song.num);
+            if(++cnt>=2)break;
         }
     }
-
     return answer;
 }
