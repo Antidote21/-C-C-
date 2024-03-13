1051c01dc36a09c61e9c60568d1959b780f21791
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-03-13 22:44:15 +0900

    카드 놓기 R

--- Baekjoon/5568.cpp
+++ Baekjoon/5568.cpp
@@ -1,30 +1,31 @@
-#include<iostream>
-#include<algorithm>
-#include<vector>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <set>
 using namespace std;
 
-int main(){
-    int n,k;
-    cin>>n>>k;
-    vector<int> Card(n);
-    vector<int> N(n);
-    for(int i=0; i<n; i++){
-        cin>>Card[i];
-    }
-    
-    for(int i=0; i<k; i++){
-        N[i]=1;
-    }
-    int cnt=0;
-    sort(Card.begin(), Card.end());
-    do{
-        for(int i=0; i<Card.size(); i++){
-            if(N[i]==1){
-                cnt++;
-            }
-        }
-        
-    }while(prev_permutation(N.begin(), N.end()));
-    cout<<cnt;
-    return 0;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n, k;
+	cin >> n >> k;
+
+	vector <int> v(n);
+
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+	sort(v.begin(), v.end());
+	
+	set <string> s;
+	do {
+		string res;
+		for (int i = 0; i < k; i++) {
+			res += to_string(v[i]);
+		}
+		s.insert(res);
+	} while (next_permutation(v.begin(), v.end()));
+	cout << s.size();
 }
