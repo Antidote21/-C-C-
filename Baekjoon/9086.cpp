f2e470b35da9438033e0383fd74713096e8d8399
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-03-14 15:00:07 +0900

    문자열 R

---
+++ Baekjoon/9086.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T; 
+
+    for (int i = 0; i < T; ++i) {
+        string str;
+        cin >> str; 
+        
+        cout << str.front() << str.back() << endl;
+    }
+
+    return 0;
+}
