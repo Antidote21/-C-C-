7edb4dbb05705f866214884a78eaeafa35ea7598
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-03-22 22:25:18 +0900

    미로 탐색 RR

--- Baekjoon/2178.cpp
+++ Baekjoon/2178.cpp
@@ -41,3 +41,54 @@ int main() {
     dfs(1, 1, 1);
     cout << Min;
 }
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int maze[100][100];
+int dist[100][100];
+int dx[4] = {0, 0, -1, 1};
+int dy[4] = {1, -1, 0, 0};
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < M; ++j) {
+            char c;
+            cin >> c;
+            maze[i][j] = c - '0';
+        }
+    }
+
+    queue<pair<int, int>> Q;
+    Q.push({0, 0});
+    dist[0][0] = 1;
+
+    while (!Q.empty()) {
+        pair<int, int> cur = Q.front();
+        Q.pop();
+
+        if (cur.first == N - 1 && cur.second == M - 1) {
+            cout << dist[cur.first][cur.second] << endl;
+            return 0;
+        }
+
+        for (int i = 0; i < 4; ++i) {
+            int xx = cur.first + dx[i];
+            int yy = cur.second + dy[i];
+
+            if (xx < 0 || xx >= N || yy < 0 || yy >= M || maze[xx][yy] == 0 || dist[xx][yy] != 0) {
+                continue;
+            }
+
+            dist[xx][yy] = dist[cur.first][cur.second] + 1;
+            Q.push({xx, yy});
+        }
+    }
+
+    return 0;
+}
