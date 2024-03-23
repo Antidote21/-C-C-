cc799c84a2974f78b7b9d3f300e75b9662313ee4
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-03-23 21:45:29 +0900

    DFS와 BFS RR

--- Baekjoon/1260.cpp
+++ Baekjoon/1260.cpp
@@ -52,3 +52,47 @@ int main(){
     cout<<endl;
     bfs(v);
 }
+
+
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<algorithm>
+using namespace std;
+
+int visited1[1001];
+int visited2[1001];
+vector<int> graph[10001];
+
+void dfs_iterative(int start){
+    stack<int> S;
+    S.push(start);
+    visited1[start] = 1;
+    while(!S.empty()){
+        int x = S.top();
+        S.pop();
+        cout << x << ' ';
+        for(int i = graph[x].size() - 1; i >= 0; --i){
+            int next = graph[x][i];
+            if(!visited1[next]){
+                S.push(next);
+                visited1[next] = 1;
+            }
+        }
+    }
+}
+
+int main(){
+    int n,m,v;
+    cin>>n>>m>>v;
+    for(int i=0; i<m; i++){
+        int a,b;
+        cin>>a>>b;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
+    for(int i=0; i<n; i++){
+        sort(graph[i].begin(), graph[i].end());
+    }
+    dfs_iterative(v);
+}
