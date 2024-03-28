0773acbec9b4f536fdcd7fc8bb99eea44ddd5c31
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-03-28 14:22:54 +0900

    바이러스 R

--- Baekjoon/2606.cpp
+++ Baekjoon/2606.cpp
@@ -37,3 +37,40 @@ int main() {
 	cout<<cnt;
 
 }
+
+
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int main(){
+    vector<int> com[100];
+    bool visited[100] = {false}; // 각 노드의 방문 여부를 저장하는 배열
+    int n, m, cnt = 0;
+    cin >> n >> m;
+    for(int i = 0; i < m; i++){
+        int a, b;
+        cin >> a >> b;
+        com[a].push_back(b);
+        com[b].push_back(a);
+    }
+    queue<int> Q;
+    Q.push(1);
+    visited[1] = true; // 시작 노드를 방문 처리
+    while(!Q.empty()){
+        int tmp = Q.front();
+        Q.pop();
+        for(int i = 0; i < com[tmp].size(); i++){
+            int neighbor = com[tmp][i];
+            if(!visited[neighbor]){ // 방문하지 않은 이웃 노드라면
+                Q.push(neighbor);
+                visited[neighbor] = true; // 방문 처리
+                cnt++;
+            }
+        }
+    }
+    cout << cnt << endl;
+    return 0;
+}
