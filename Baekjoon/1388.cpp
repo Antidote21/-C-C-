a2f57f2103c15c9ae3b2e41aaccc422d9248f2d4
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-04-04 23:22:38 +0900

    바닥 장식 C

--- Baekjoon/1388.cpp
+++ Baekjoon/1388.cpp
@@ -1,30 +1,28 @@
 #include <iostream>
-#include <vector>
-#include <queue>
 
 using namespace std;
 
+char A[100][100];
 int main(){
     int N, M;
     cin>>N>>M;
-    char A[50][50];
-    queue<pair<int, int> > Q;
-
+    int cnt=0;
     for(int i=0; i<N; i++){
         for(int j=0; j<M; j++){
             cin>>A[i][j];
-            Q.push({i,j});
+            cnt++;
         }
     }
+
     for(int i=0; i<N; i++){
-            for(int j=0; j<M; j++){
-                if(A[i][j]=='-'&&A[i][j+1]=='-'){
-                    Q.pop();
-                }
-                if(A[i][j]=='|'&&A[i+1][j]=='|'){
-                    Q.pop();
-                }
+        for(int j=0; j<M; j++){
+            if(A[i][j]=='-'&&A[i][j+1]=='-'){
+                cnt--;
+            }
+            if(A[i][j]=='|'&&A[i+1][j]=='|'){
+                cnt--;
             }
         }
-    cout<<Q.size();
+    }
+    cout<<cnt;
 }
