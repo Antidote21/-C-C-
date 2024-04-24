42476686c4bbc0aa66a5046b82163bb24d426449
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-04-24 23:50:08 +0900

    문자열 C

--- Baekjoon/9086.cpp
+++ Baekjoon/9086.cpp
@@ -16,3 +16,18 @@ int main() {
 
     return 0;
 }
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int T;
+    cin>>T;
+    for(int i=0; i<T; i++){
+        string s;
+        cin>>s;
+        cout<<s[0]<<s[s.size()-1]<<endl;
+    }
+}
