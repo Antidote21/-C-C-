179c37b76ad021e08b4aa577bf7cf29757d3df80
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-04-26 23:26:47 +0900

    퇴사 RR

--- Baekjoon/14501.cpp
+++ Baekjoon/14501.cpp
@@ -4,31 +4,22 @@
 
 using namespace std;
 
-int max_profit(int N, vector<pair<int, int>>& schedules) {
+int main(){
+    int N, Ti, Pi;
+    cin >> N;
+    vector<pair<int, int>> schedule(N);
     vector<int> dp(N + 1, 0);
-    for (int i = N - 1; i >= 0; i--) {
-        if (i + schedules[i].first <= N) {  
-            dp[i] = max(schedules[i].second + dp[i + schedules[i].first], dp[i + 1]);
-        } else {  
+    for(int i = 0; i < N; i++){
+        cin >> Ti >> Pi;
+        schedule[i] = {Ti, Pi};
+    }
+    
+    for(int i = N - 1; i >= 0; i--){
+        if(i + schedule[i].first <= N){
+            dp[i] = max(schedule[i].second + dp[i + schedule[i].first], dp[i + 1]);
+        }else{
             dp[i] = dp[i + 1];
         }
     }
-
-    return dp[0];
-}
-
-int main() {
-    int N;
-    cin >> N;
-
-    vector<pair<int, int>> schedules(N);
-    for (int i = 0; i < N; i++) {
-        int Ti, Pi;
-        cin >> Ti >> Pi;
-        schedules[i] = make_pair(Ti, Pi);
-    }
-
-    cout << max_profit(N, schedules) << endl;
-
-    return 0;
+    cout << dp[0] << endl;
 }
