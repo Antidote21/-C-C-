d4c360ca6869fcc08ee5b81cf2ebb49ad465ec3c
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-04-27 23:33:20 +0900

    줄세우기 RR

--- Baekjoon/2631.cpp
+++ Baekjoon/2631.cpp
@@ -28,3 +28,33 @@ int main() {
 
     return 0;
 }
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int N;
+    cin>>N;
+    vector<int> row(N);
+
+    for(int i=0; i<N; i++){
+        cin>>row[i];
+    }
+    
+    vector<int> dp(N);
+    int res = 0;
+    
+    for(int i=0; i<N; i++){
+        dp[i] = 1;
+        for(int j=0; j<i; j++){
+            if(row[i] > row[j] && dp[i] < dp[j]+1){
+                dp[i]++;
+            }
+        }
+        if(res<dp[i])res=dp[i];
+    }
+    cout<<N-res<<'\n';
+    return 0;
+}
