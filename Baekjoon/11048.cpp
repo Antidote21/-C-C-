74b3c9c27c3be9caf997a856a2b42d9d1cb66b7f
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-05-02 23:19:08 +0900

    이동하기 R

--- Baekjoon/11048.cpp
+++ Baekjoon/11048.cpp
@@ -8,23 +8,21 @@ int dp[1001][1001];
 
 int main(){
     int N, M;
-    cin >> N >> M;
-    
-    for(int i = 1; i <= N; i++){
-        for(int j = 1; j <= M; j++){
-            cin >> map[i][j];
+    cin>>N>>M;
+    for(int i=1; i<=N; i++){
+        for(int j=1; j<=M; j++){
+            cin>>map[i][j];
+        }
+    }
+    for(int i=1; i<=N; i++){
+        for(int j=1; j<=M; j++){
+            dp[i][j] = max(dp[i][j], dp[i-1][j]+map[i][j]);
+            dp[i][j] = max(dp[i][j], dp[i][j-1]+map[i][j]);
+            dp[i][j] = max(dp[i][j], dp[i-1][j-1]+map[i][j]);
         }
     }
     
-    for(int i = 1; i <= N; i++){
-        for(int j = 1; j <= M; j++){
-            dp[i][j] = max(dp[i][j], dp[i-1][j] + map[i][j]);
-            dp[i][j] = max(dp[i][j], dp[i][j-1] + map[i][j]);
-            dp[i][j] = max(dp[i][j], dp[i-1][j-1] + map[i][j]);
-        }
-    }
-    
-    cout << dp[N][M];
+    cout<<dp[N][M];
     
     return 0;
 }
