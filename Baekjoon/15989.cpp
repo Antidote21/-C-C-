ba554fe05284b8990739fb306d1dbeaaea5c0676
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-05-05 23:46:49 +0900

    1, 2, 3 더하기 4 R

--- Baekjoon/15989.cpp
+++ Baekjoon/15989.cpp
@@ -1,42 +1,29 @@
 #include <iostream>
-#include <cstring>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int N;
 int dp[10001][4];
 
-void solution() {
-    dp[1][1] = 1;
-
-    dp[2][1] = 1;
-    dp[2][2] = 1;
-
-    dp[3][1] = 1;
-    dp[3][2] = 1;
-    dp[3][3] = 1;
-
-    for (int i = 4; i <= 10000; i++) {
-        dp[i][1] = 1;
-        dp[i][2] = dp[i - 2][1] + dp[i - 2][2];
-        dp[i][3] = dp[i - 3][1] + dp[i - 3][2] + dp[i - 3][3];
+int main(){
+    int T, N;
+    cin>>T;
+    
+    for(int i=1; i<=3; i++){
+        for(int j=1; j<=i; j++){
+            dp[i][j] = 1;
+        }
     }
-
-}
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-
-    int T;
-    cin >> T;
-
-    solution();
-
+    
+    for(int i=4; i<=10000; i++){
+        dp[i][1] = 1;
+        dp[i][2] = dp[i-2][1]+dp[i-2][2];
+        dp[i][3] = dp[i-3][1]+dp[i-3][2]+dp[i-3][3];
+    }
+    
     while (T--) {
         cin >> N;
         cout << dp[N][1] + dp[N][2] + dp[N][3] << endl;
     }
-
-    return 0;
 }
