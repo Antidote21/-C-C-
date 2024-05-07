7f6e6bf7a98a593ed04e8761890ad4e095db3158
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-05-07 23:31:28 +0900

    ATM RR

--- Baekjoon/11399.cpp
+++ Baekjoon/11399.cpp
@@ -24,3 +24,31 @@ int main(){
     cout<<total_time<<'\n';
     
 }
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+
+int main(){    
+    cin>>n;
+    vector<pair<int, int>> res;
+    for(int i=0; i<n; i++){
+        int p;
+        cin>>p;
+        res.push_back(make_pair(p,i));
+    }
+    sort(res.begin(), res.end());
+    
+    int total_time = 0;
+    for(int i=0; i<n; i++){
+        for(int j=i; j>=0; j--){
+            total_time += res[j].first;
+        }
+    }
+    cout<<total_time<<endl;
+    
+}
