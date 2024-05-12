0abd439c4f23c4f6214f9a18ea94ec34f7dcdf1a
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-05-12 23:50:42 +0900

    동전 1 R

--- Baekjoon/2293.cpp
+++ Baekjoon/2293.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
-int main() {
+int main(){
     int n, k;
-    cin >> n >> k;
-
-    vector<int> coin(n, 0);
-    vector<int> dp(k + 1, 0);
-
-    for (int i = 0; i < n; i++) {
-        cin >> coin[i];
+    cin>>n>>k;
+    
+    vector<int> coin(n);
+    vector<int> dp(k+1);
+    
+    for(int i=0; i<n; i++){
+        cin>>coin[i];
     }
-
+    
     dp[0] = 1;
-
-    for (int i = 0; i < n; i++) {
-        for (int j = coin[i]; j <= k; j++) {
-            dp[j] += dp[j - coin[i]];
+    
+    for(int i=0; i<n; i++){
+        for(int j=coin[i]; j<=k; j++){
+            dp[j] += dp[j-coin[i]];
         }
     }
-
-    cout << dp[k] << endl;
+    cout<<dp[k]<<endl;
     return 0;
 }
