e1fb0b82ed6d3c592b0470f8cf816df5bc5cc12f
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-05-17 22:55:55 +0900

    팰린드롬인지 확인하기 C

--- Baekjoon/10988.cpp
+++ Baekjoon/10988.cpp
@@ -14,3 +14,23 @@ int main(){
     }
     cout<<"1";
 }
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+    int cnt=0;
+    
+    for(int i=0; i<s.length()/2; i++){
+        if(s[i]==s[s.length()-1-i]){
+            cnt++;
+        }
+    }
+    
+    if(cnt==s.length()/2)cout<<1;
+    else cout<<0;
+}
