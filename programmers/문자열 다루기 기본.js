8addeb721b8bca4236646424d180a9c541ca330c
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-05-30 00:01:30 +0900

    코딩테스트 연습 연습문제 C

--- programmers/문자열 다루기 기본.js
+++ programmers/문자열 다루기 기본.js
@@ -1,6 +1,7 @@
 function solution(s) {
-    var answer = true;
+    var answer = false;
     var len = s.length;
+    if(len == 4 || len == 6)answer = true;
     for(var i=0; i<len; i++){
         if(s[i]>='A'&&s[i]<='z'){
            answer = false;
