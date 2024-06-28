c80796415078542e81a7ce669f3e0ab38949b3e1
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-06-29 02:03:08 +0900

    탐욕법(Greedy) RR

--- programmers/구명보트.cpp
+++ programmers/구명보트.cpp
@@ -20,3 +20,28 @@ int solution(vector<int> people, int limit) {
     }
     return answer;
 }
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 0;
+    sort(people.begin(), people.end()); 
+    int i = 0; 
+    int j = people.size() - 1;
+    
+    while (i <= j) { 
+        if (people[i] + people[j] <= limit) {
+            i++;
+            j--;
+        } else {
+            j--;
+        }
+        answer++; 
+    }
+    
+    return answer;
+}
