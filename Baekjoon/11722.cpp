c4c7ca14a9c1fce574954c0f1e8ea361fc5122f3
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-07-05 22:43:43 +0900

    가장 긴 감소하는 부분 수열 R

--- Baekjoon/11722.cpp
+++ Baekjoon/11722.cpp
@@ -30,3 +30,32 @@ int main() {
     cout << len << endl;
     return 0;
 }
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> A(n);
+    for(int i = 0; i < n; i++){
+        cin >> A[i];
+    }
+    vector<int> dp(n, 1);
+
+    for(int i = 1; i < n; i++){
+        for(int j = 0; j < i; j++){
+            if(A[j] > A[i] && dp[j] + 1 > dp[i]){
+                dp[i] = dp[j] + 1;
+            }
+        }
+    }
+
+    int max_len = *max_element(dp.begin(), dp.end());
+    cout << max_len << endl;
+
+    return 0;
+}
