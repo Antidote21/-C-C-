1cf5f7fbda425fa6ddbab29b8825c672eb212e4b
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-07-07 21:45:58 +0900

    1,2,3 더하기 C

--- Baekjoon/9095.cpp
+++ Baekjoon/9095.cpp
@@ -27,3 +27,30 @@ int main() {
     
     return 0;
 }
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int T;
+    cin>>T;
+    vector<int> A(T);
+    vector<int> dp(11);
+    for(int i=0; i<T; i++){
+        cin>>A[i];
+    }
+    
+    dp[1]=1;
+    dp[2]=2;
+    dp[3]=4;
+
+    for(int i=4; i<11; i++){
+        dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
+    }
+    
+    for(int i=0; i<T; i++){
+        cout<<dp[A[i]]<<endl;
+    }
+}
