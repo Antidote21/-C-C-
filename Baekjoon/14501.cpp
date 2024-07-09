41194112372e066d992b31f6eafbed6e8ed11b84
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-07-09 23:02:51 +0900

    퇴사 ㄲㄲ

--- Baekjoon/14501.cpp
+++ Baekjoon/14501.cpp
@@ -23,3 +23,35 @@ int main(){
     }
     cout << dp[0] << endl;
 }
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    vector<pair<int, int>> TP(N);
+    vector<int> dp(N+1, 0); 
+
+    for(int i = 0; i < N; i++){
+        int T, P;
+        cin >> T >> P;
+        TP[i] = {T, P};
+    }
+
+    for(int i = 0; i < N; i++){
+        int T = TP[i].first;
+        int P = TP[i].second;
+        if (i + T <= N) {
+            dp[i + T] = max(dp[i + T], dp[i] + P);
+        }
+        dp[i + 1] = max(dp[i + 1], dp[i]);
+    }
+
+    cout << *max_element(dp.begin(), dp.end()) << endl;
+
+    return 0;
+}
