9a5ecccdeb19233db9dbb331ad0f8b468097d069
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-07-10 22:07:30 +0900

    카드 구매하기 RR

--- Baekjoon/11052.cpp
+++ Baekjoon/11052.cpp
@@ -24,3 +24,29 @@ int main() {
 
     return 0;
 }
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> Card(n+1);
+    vector<int> dp(n+1, 0);
+    
+    for(int i=1; i<=n; i++){
+        cin>>Card[i];
+    }
+    
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            dp[i] = max(dp[i], dp[i-j]+Card[j]);
+        }
+    }
+
+    cout<<dp[n];
+}
