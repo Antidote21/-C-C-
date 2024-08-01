e0aea1a285244470ea2c2c3c8f8c075b289788d7
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-08-01 17:28:05 +0900

    수 찾기 C

--- Baekjoon/1920.cpp
+++ Baekjoon/1920.cpp
@@ -25,3 +25,45 @@ int main(){
     }
     return 0;
 }
+
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int N;
+    cin>>N;
+    unordered_map<int, int> A;
+    vector<int> B;
+
+    for(int i=0; i<N; i++){
+        int p;
+        cin>>p;
+        A.insert({p,i});
+    }
+    
+    int M;
+    cin>>M;
+    for(int i=0; i<M; i++){
+        int q; 
+        cin>>q;
+        B.push_back(q);
+    }
+    
+    string result;
+    for(auto elem:B){
+        if(A.count(elem)){
+            result += "1\n";
+        }else{
+            result += "0\n";
+        }
+    }
+    cout << result;
+  
+    return 0;
+}
