56ab778c3f744c41a7e8ddc6e51215d53e3abc03
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-08-06 14:35:16 +0900

    해시 R

--- programmers/의상.cpp
+++ programmers/의상.cpp
@@ -19,3 +19,30 @@ int solution(vector<vector<string>> clothes) {
 
     return answer-1;
 }
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 1;
+    
+    unordered_map<string, int> um;
+    
+    for(int i = 0; i < clothes.size(); i++) {
+        string type = clothes[i][1]; // 의상의 종류를 키로 사용
+        if(um[type] >= 1) {
+            um[type]++;
+        } else {
+            um[type] = 1; 
+        }
+    }
+    
+    for(auto elem : um) {
+        answer *= (elem.second + 1); 
+    }
+
+    return answer - 1; 
+}
