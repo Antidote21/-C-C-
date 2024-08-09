0f6d9ea40ad68d201233ae4802bd85aa5ffd1137
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-08-09 14:28:19 +0900

    스택 C

--- programmers/같은 숫자는 싫어.cpp
+++ programmers/같은 숫자는 싫어.cpp
@@ -24,3 +24,32 @@ vector<int> solution(vector<int> arr)
     reverse(answer.begin(), answer.end());
     return answer;
 }
+
+#include <vector>
+#include <iostream>
+#include <stack>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> arr) 
+{
+    vector<int> answer;
+    stack<int> st;
+    
+    for(auto elem:arr){
+        if(st.empty()){st.push(elem);}
+        if(st.top() != elem){
+            st.push(elem);
+        }
+    }
+
+    while(true){
+        if(st.empty())break;
+        int tmp;
+        tmp = st.top();
+        answer.push_back(tmp);
+        st.pop();
+    }
+    reverse(answer.begin(), answer.end());
+    return answer;
+}
