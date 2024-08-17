c323208b31921b2db0b0ef952bbe9a2e7a74952e
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-08-17 21:27:03 +0900

    스택/큐 RR

--- programmers/다리를 지나는 트럭.cpp
+++ programmers/다리를 지나는 트럭.cpp
@@ -42,23 +42,30 @@ using namespace std;
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
     int answer = 0;
-    int next_truck = 0;//대기트럭중맨앞의트럭의인덱스
-    int weight_sum = 0;//다리위에있는트럭의무게합
-    queue<int> on_bridge;//다리위에있는트럭
-
-    for(int i=0; i<truck_weights; i++){
-        on_bridge.push(truck_weights[i]);
+    int weight_sum = 0;
+    int next_truck = 0;
+    queue<int> on_bridge;
+    
+    for(int i=0; i<bridge_length; i++){
+        on_bridge.push(0);
     }
     
     while(!on_bridge.empty()){
-        int tmp = on_bridge.front();
+        answer++;
+        
+        weight_sum -= on_bridge.front();
         on_bridge.pop();
-        if(weight_sum+tmp<weight){
-            weight_sum+=truck_weights[next_truck];
-            next_truck++;
+        
+        if(next_truck < truck_weights.size()){
+            if(weight_sum + truck_weights[next_truck] <= weight){
+                on_bridge.push(truck_weights[next_truck]);
+                weight_sum += truck_weights[next_truck];
+                next_truck++;
+            }else{
+                on_bridge.push(0);
+            }
         }
-        answer += bridge_length;
     }
-    
     return answer;
 }
+
