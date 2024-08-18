ee23fe55e939d86f9b184adb8101bfec4c2b213d
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-08-18 21:07:03 +0900

    스택/큐 RR

--- programmers/주식가격.cpp
+++ programmers/주식가격.cpp
@@ -43,5 +43,28 @@ vector<int> solution(vector<int> prices) {
             }
         answer.push_back(chk); 
     }
+
+    #include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    vector<int> answer(prices.size(), 0);
+    stack<int> st;
+    for(int i=0; i<prices.size(); i++){
+        while(!st.empty() && prices[st.top()] > prices[i]){
+            answer[st.top()] = i - st.top();
+            st.pop();
+        }
+        st.push(i);
+    }
+    while(!st.empty()){
+        answer[st.top()] = prices.size() - st.top() -1;
+        st.pop();
+    }
+    return answer;
+}
     return answer;
 }
