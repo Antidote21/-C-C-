9989e5a9e491a18a482d7a5f9664b1bcc8667e3f
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-08-20 19:56:40 +0900

    힙 RR

--- programmers/디스크 컨트롤러.cpp
+++ programmers/디스크 컨트롤러.cpp
@@ -33,3 +33,47 @@ int solution(vector<vector<int>> jobs) {
     
     return answer / jobs.size(); 
 }
+
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+struct compare {
+    bool operator()(const pair<int, int>& a, const pair<int, int>& b) {
+        return a.second > b.second; 
+    }
+};
+
+int solution(vector<vector<int>> jobs) {
+    int answer = 0;
+    int time = 0; 
+    int index = 0;
+    int total_time = 0; 
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, compare> pq;
+
+    sort(jobs.begin(), jobs.end());
+
+    while (index < jobs.size() || !pq.empty()) {
+        while (index < jobs.size() && jobs[index][0] <= time) {
+            pq.push({jobs[index][0], jobs[index][1]});
+            index++;
+        }
+
+        if (!pq.empty()) {
+            pair<int, int> cur = pq.top();
+            pq.pop();
+
+            time += cur.second; 
+            total_time += (time - cur.first); 
+        } else {
+            time = jobs[index][0]; 
+        }
+    }
+
+    answer = total_time / jobs.size(); 
+    return answer;
+}
