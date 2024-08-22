649d2f64356ec7c0f49744c9af5323a23a55d716
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-08-22 14:40:05 +0900

    정렬 R

--- programmers/K번째수.cpp
+++ programmers/K번째수.cpp
@@ -14,3 +14,28 @@ vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     }
     return answer;
 }
+
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    if(array.size()==1){
+        answer.push_back(array[0]);
+        return answer;
+    }
+    for(int i = 0; i < commands.size(); i++) {
+        int p = commands[i][0];
+        int q = commands[i][1];
+        int r = commands[i][2];
+
+        vector<int> tmp(array.begin() + (p - 1), array.begin() + q);
+        sort(tmp.begin(), tmp.end());
+        answer.push_back(tmp[r - 1]);
+    }
+    
+    return answer;
+}
