e11d10e56d852b3e357b30e50ecca91cb95b353a
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-08-23 23:50:32 +0900

    가장 큰 수 RR

--- programmers/가장 큰 수.cpp
+++ programmers/가장 큰 수.cpp
@@ -25,3 +25,40 @@ string solution(vector<int> numbers) {
 
     return answer;
 }
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// 비교 함수 정의
+bool compare(string a, string b) {
+    return a + b > b + a;
+}
+
+string solution(vector<int> numbers) {
+    vector<string> str_numbers;
+    
+    // 정수를 문자열로 변환하여 벡터에 저장
+    for(int num : numbers) {
+        str_numbers.push_back(to_string(num));
+    }
+    
+    // 비교 함수에 따라 정렬
+    sort(str_numbers.begin(), str_numbers.end(), compare);
+    
+    // 정렬된 문자열들을 이어붙임
+    string answer = "";
+    for(string str : str_numbers) {
+        answer += str;
+    }
+    
+    // 모든 숫자가 0인 경우 "0"을 반환
+    if(answer[0] == '0') {
+        return "0";
+    }
+    
+    return answer;
+}
+
