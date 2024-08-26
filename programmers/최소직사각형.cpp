6280da0117f29a6a5b7d6b7da2d0f4fd1a9ca882
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-08-26 16:33:28 +0900

    완전 탐색 RR

--- programmers/최소직사각형.cpp
+++ programmers/최소직사각형.cpp
@@ -14,3 +14,24 @@ int solution(vector<vector<int>> sizes) {
 
     return max_width * max_height;
 }
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<vector<int>> sizes) {
+    int max_width = 0;
+    int max_height = 0;
+
+    for (auto& size : sizes) {
+        if (size[0] < size[1]) {
+            swap(size[0], size[1]);
+        }
+        max_width = max(max_width, size[0]);
+        max_height = max(max_height, size[1]);
+    }
+
+    return max_width * max_height;
+}
