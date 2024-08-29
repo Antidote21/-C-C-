8e26cd563b7714212df4740aa0f649d32471c460
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-08-29 23:54:46 +0900

    완전 탐색 ㄲㄲ

--- programmers/카펫.cpp
+++ programmers/카펫.cpp
@@ -41,3 +41,23 @@ vector<int> solution(int brown, int red) {
 
     return vector<int>{w, h};
 }
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int brown, int yellow) {
+    vector<int> answer;
+
+    for (int height = 1; height <= (brown + yellow) / height; height++) {
+        int width = (brown + yellow) / height;
+        if ((width - 2) * (height - 2) == yellow) {
+            answer.push_back(width);
+            answer.push_back(height);
+            break;
+        }
+    }
+
+    return answer;
+}
