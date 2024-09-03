56944513f46c83602505f6b292d5711068291f44
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-09-03 23:47:22 +0900

    탐욕법 RR

--- programmers/큰 수 만들기.cpp
+++ programmers/큰 수 만들기.cpp
@@ -55,3 +55,40 @@ string solution(string number, int k) {
     answer = result[0];
     return answer;
 }
+
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+string solution(string number, int k) {
+    string answer = "";
+    int n = number.size();
+    stack<char> s;
+
+    for (int i = 0; i < n; i++) {
+        char c = number[i];
+        // 스택의 top에 있는 문자보다 현재 문자가 크면 스택의 top을 pop하고 k를 줄인다.
+        while (!s.empty() && s.top() < c && k > 0) {
+            s.pop();
+            k--;
+        }
+        // 스택에 현재 문자를 push한다.
+        s.push(c);
+    }
+    
+    // k가 남아있다면 스택의 top에서 k만큼 더 pop한다.
+    while (k > 0) {
+        s.pop();
+        k--;
+    }
+
+    // 스택의 문자들을 모아 답을 만든다.
+    while (!s.empty()) {
+        answer = s.top() + answer;
+        s.pop();
+    }
+
+    return answer;
+}
