9407827e72a81e8dd9a15c911ff59a1e9d25bc5a
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-09-23 22:27:16 +0900

    완전 탐색 RR

--- programmers/피로도.cpp
+++ programmers/피로도.cpp
@@ -29,3 +29,35 @@ int solution(int k, vector<vector<int>> dungeons) {
 
     return answer;
 }
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int k, vector<vector<int>> dungeons) {
+    int answer = 0;
+    vector<int> v;
+
+    for (int i = 0; i < dungeons.size(); i++) {
+        v.push_back(i);
+    }
+
+    do {
+        int tmp = k;
+        int cnt = 0;
+
+        for (int i = 0; i < v.size(); i++) {
+            int j = v[i];
+            if (tmp >= dungeons[j][0]) {
+                tmp -= dungeons[j][1];
+                cnt++;
+            }
+        }
+
+        answer = max(answer, cnt);
+    } while (next_permutation(v.begin(), v.end()));
+
+    return answer;
+}
