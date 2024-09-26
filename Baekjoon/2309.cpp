7c2d1ae50bb0aca953174d2f4b3cc13bdad3bf06
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2024-09-26 22:44:43 +0900

    일곱난쟁이 RR

--- Baekjoon/2309.cpp
+++ Baekjoon/2309.cpp
@@ -63,3 +63,42 @@ int main(){
     }
     return 0;
 }
+
+#include <iostream>
+#include <vector>
+#include <algorithm> 
+
+using namespace std;
+
+int height[10];
+int ch[10];
+vector<int> result(7); 
+
+void dfs(vector<int> res, int cnt, int sum, int idx){ 
+    if(cnt==7){ 
+        if(sum==100){ 
+            result = res; 
+            return;
+        }
+    }
+    if(cnt > 7 || idx >= 9) return; 
+    
+    res.push_back(height[idx]);
+    dfs(res, cnt+1, sum+height[idx], idx+1); 
+    res.pop_back(); 
+    
+    dfs(res, cnt, sum, idx+1); 
+}
+
+int main(){
+    for(int i=0; i<9; i++){
+        cin >> height[i];
+    }
+    sort(height, height + 9); 
+    vector<int> temp; 
+    dfs(temp, 0, 0, 0); 
+    for(auto k:result){
+        cout << k << endl;
+    }
+    return 0;
+}
